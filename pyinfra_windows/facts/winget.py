b6231f8ceb386f1ef847702b810269686816b8e8
Ricky Dalexis <rod7760@rit.edu>
2025-08-28 00:48:35 -0400

    Add WingetPackages fact

---
+++ pyinfra_windows/facts/winget.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing_extensions import override
+
+from pyinfra.api import FactBase
+
+from pyinfra.facts.util.packaging import parse_packages
+
+WINGET_REGEX = r"([^\s]+)\s+([^\s]+)"
+
+
+class WingetPackages(FactBase):
+    """ Returns a dict of installed winget packages:
+
+    .. code:: python
+
+        {
+            "package_name": ["version"],
+        }
+    """
+
+    @override
+    def command(self) -> str:
+        return "Get-WingetPackage | Select -Property Id, InstalledVersion"
+
+    shell_executable = "ps"
+
+    default = dict
+
+    @override
+    def process(self, output):
+        packages = parse_packages(WINGET_REGEX, output)
+        return packages
